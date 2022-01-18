0babacde27f703dd8de635f37f51cad628c97623
Andreas Pfeil <andreas.pfeil@kit.edu>
2022-01-18 19:13:07 +0100

    Improve integration tests.

---
+++ src/test/java/edu/kit/datamanager/pit/web/RestWithHandleProtocolTest.java
@@ -0,0 +1,95 @@
+package edu.kit.datamanager.pit.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import edu.kit.datamanager.pit.domain.PIDRecord;
+import edu.kit.datamanager.pit.pidsystem.impl.HandleProtocolAdapter;
+import edu.kit.datamanager.pit.pidsystem.impl.InMemoryIdentifierSystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import javax.servlet.ServletContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// JUnit5 + Spring
+@ExtendWith(SpringExtension.class)
+// Might be needed for WebApp testing according to
+// https://www.baeldung.com/integration-testing-in-spring
+// @WebAppConfiguration
+// Default preparation foo for mockMVC
+@AutoConfigureMockMvc
+// Set the in-memory implementation
+@SpringBootTest(properties = {
+        "pit.pidsystem.implementation = HANDLE_PROTOCOL"
+})
+@TestPropertySource("/test/application-test.properties")
+// TODO why a testing profile?
+@ActiveProfiles("test")
+public class RestWithHandleProtocolTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper;
+
+    static final String pid = "21.T11148/076759916209e5d62bd5";
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+        this.mapper = this.webApplicationContext.getBean("OBJECT_MAPPER_BEAN", ObjectMapper.class);
+    }
+
+    @Test
+    public void checkTestSetup() {
+        assertNotNull(this.mockMvc);
+        assertNotNull(this.webApplicationContext);
+        ServletContext servletContext = webApplicationContext.getServletContext();
+        
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(webApplicationContext.getBean(ITypingRestResource.class));
+        assertNotNull(webApplicationContext.getBean(HandleProtocolAdapter.class));
+        assertThrows(NoSuchBeanDefinitionException.class, () -> {
+            webApplicationContext.getBean(InMemoryIdentifierSystem.class);
+        });
+    }
+
+    @Test
+    public void resolveSomething() throws Exception {
+        MvcResult resolved = this.mockMvc
+            .perform(
+                get("/api/v1/pit/pid/".concat(pid))
+            )
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+        
+        String resolvedBody = resolved.getResponse().getContentAsString();
+        PIDRecord resolvedRecord = mapper.readValue(resolvedBody, PIDRecord.class);
+        assertEquals(resolvedRecord.getPid(), pid);
+    }
+}
--- src/test/java/edu/kit/datamanager/pit/web/RestWithInMemoryTest.java
+++ src/test/java/edu/kit/datamanager/pit/web/RestWithInMemoryTest.java
@@ -2,19 +2,26 @@ package edu.kit.datamanager.pit.web;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.servlet.ServletContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.web.context.WebApplicationContext;
+
+import edu.kit.datamanager.pit.domain.PIDRecord;
+import edu.kit.datamanager.pit.pidsystem.impl.HandleProtocolAdapter;
+import edu.kit.datamanager.pit.pidsystem.impl.InMemoryIdentifierSystem;
 
 // org.springframework.mock is for unit testing
 // Source: https://docs.spring.io/spring-framework/docs/current/reference/html/testing.html
@@ -24,22 +31,19 @@ import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 // JUnit5 + Spring
 @ExtendWith(SpringExtension.class)
-// TODO Might be needed for WebApp testing according to https://www.baeldung.com/integration-testing-in-spring
-@WebAppConfiguration
+// Might be needed for WebApp testing according to https://www.baeldung.com/integration-testing-in-spring
+//@WebAppConfiguration
 // Default preparation foo for mockMVC
 @AutoConfigureMockMvc
 // Set the in-memory implementation
@@ -48,19 +52,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 // TODO why a testing profile?
 @ActiveProfiles("test")
 public class RestWithInMemoryTest {
+
+    static final String EMPTY_RECORD = "{\"pid\": null, \"entries\": {}}";
+    static final String RECORD = "{\"entries\":{\"21.T11148/076759916209e5d62bd5\":[{\"key\":\"21.T11148/076759916209e5d62bd5\",\"name\":\"kernelInformationProfile\",\"value\":\"21.T11148/301c6f04763a16f0f72a\"}],\"21.T11148/397d831aa3a9d18eb52c\":[{\"key\":\"21.T11148/397d831aa3a9d18eb52c\",\"name\":\"dateModified\",\"value\":\"2021-12-21T17:36:09.541+00:00\"}],\"21.T11148/8074aed799118ac263ad\":[{\"key\":\"21.T11148/8074aed799118ac263ad\",\"name\":\"digitalObjectPolicy\",\"value\":\"21.T11148/37d0f4689c6ea3301787\"}],\"21.T11148/92e200311a56800b3e47\":[{\"key\":\"21.T11148/92e200311a56800b3e47\",\"name\":\"etag\",\"value\":\"{ \\\"sha256sum\\\": \\\"sha256 c50624fd5ddd2b9652b72e2d2eabcb31a54b777718ab6fb7e44b582c20239a7c\\\" }\"}],\"21.T11148/aafd5fb4c7222e2d950a\":[{\"key\":\"21.T11148/aafd5fb4c7222e2d950a\",\"name\":\"dateCreated\",\"value\":\"2021-12-21T17:36:09.541+00:00\"}],\"21.T11148/b8457812905b83046284\":[{\"key\":\"21.T11148/b8457812905b83046284\",\"name\":\"digitalObjectLocation\",\"value\":\"https://test.repo/file001\"}],\"21.T11148/c692273deb2772da307f\":[{\"key\":\"21.T11148/c692273deb2772da307f\",\"name\":\"version\",\"value\":\"1.0.0\"}],\"21.T11148/c83481d4bf467110e7c9\":[{\"key\":\"21.T11148/c83481d4bf467110e7c9\",\"name\":\"digitalObjectType\",\"value\":\"21.T11148/ManuscriptPage\"}]},\"pid\":\"unregistered-18622\"}";
     
     @Autowired
     private WebApplicationContext webApplicationContext;
 
     private MockMvc mockMvc;
+
+    private ObjectMapper mapper;
     
     @BeforeEach
     public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+        this.mapper = this.webApplicationContext.getBean("OBJECT_MAPPER_BEAN", ObjectMapper.class);
     }
-
-    static final String EMPTY_RECORD = "{\"pid\": null, \"entries\": {}}";
-    static final String RECORD = "{\"entries\":{\"21.T11148/076759916209e5d62bd5\":[{\"key\":\"21.T11148/076759916209e5d62bd5\",\"name\":\"kernelInformationProfile\",\"value\":\"21.T11148/301c6f04763a16f0f72a\"}],\"21.T11148/397d831aa3a9d18eb52c\":[{\"key\":\"21.T11148/397d831aa3a9d18eb52c\",\"name\":\"dateModified\",\"value\":\"2021-12-21T17:36:09.541+00:00\"}],\"21.T11148/8074aed799118ac263ad\":[{\"key\":\"21.T11148/8074aed799118ac263ad\",\"name\":\"digitalObjectPolicy\",\"value\":\"21.T11148/37d0f4689c6ea3301787\"}],\"21.T11148/92e200311a56800b3e47\":[{\"key\":\"21.T11148/92e200311a56800b3e47\",\"name\":\"etag\",\"value\":\"{ \\\"sha256sum\\\": \\\"sha256 c50624fd5ddd2b9652b72e2d2eabcb31a54b777718ab6fb7e44b582c20239a7c\\\" }\"}],\"21.T11148/aafd5fb4c7222e2d950a\":[{\"key\":\"21.T11148/aafd5fb4c7222e2d950a\",\"name\":\"dateCreated\",\"value\":\"2021-12-21T17:36:09.541+00:00\"}],\"21.T11148/b8457812905b83046284\":[{\"key\":\"21.T11148/b8457812905b83046284\",\"name\":\"digitalObjectLocation\",\"value\":\"https://test.repo/file001\"}],\"21.T11148/c692273deb2772da307f\":[{\"key\":\"21.T11148/c692273deb2772da307f\",\"name\":\"version\",\"value\":\"1.0.0\"}],\"21.T11148/c83481d4bf467110e7c9\":[{\"key\":\"21.T11148/c83481d4bf467110e7c9\",\"name\":\"digitalObjectType\",\"value\":\"21.T11148/ManuscriptPage\"}]},\"pid\":\"unregistered-18622\"}";
 
 
     @Test
@@ -72,6 +79,10 @@ public class RestWithInMemoryTest {
         assertNotNull(servletContext);
         assertTrue(servletContext instanceof MockServletContext);
         assertNotNull(webApplicationContext.getBean(ITypingRestResource.class));
+        assertNotNull(webApplicationContext.getBean(InMemoryIdentifierSystem.class));
+        assertThrows(NoSuchBeanDefinitionException.class, () -> {
+            webApplicationContext.getBean(HandleProtocolAdapter.class);
+        });
     }
 
     @Test
@@ -100,8 +111,9 @@ public class RestWithInMemoryTest {
 
     @Test
     public void testCreateValidRecord() throws Exception {
+        // test create
         assertEquals(this.webApplicationContext.getEnvironment().getProperty("repo.messaging.enabled"), "false");
-        MvcResult result = this.mockMvc
+        MvcResult created = this.mockMvc
             .perform(
                 post("/api/v1/pit/pid/")
                     .contentType(MediaType.APPLICATION_JSON)
@@ -112,8 +124,24 @@ public class RestWithInMemoryTest {
             .andDo(MockMvcResultHandlers.print())
             .andExpect(MockMvcResultMatchers.status().isCreated())
             .andReturn();
+        
+        String createdBody = created.getResponse().getContentAsString();
+        PIDRecord createdRecord = mapper.readValue(createdBody, PIDRecord.class);
+        String createdPid = createdRecord.getPid();
+        assertTrue(createdPid.contains("sandboxed/"));
 
-        String record = result.getResponse().getContentAsString();
-        assertTrue(record.contains("sandboxed/"));
+        // test resolve
+        MvcResult resolved = this.mockMvc
+            .perform(
+                get("/api/v1/pit/pid/".concat(createdPid))
+            )
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+        
+        String resolvedBody = resolved.getResponse().getContentAsString();
+        PIDRecord resolvedRecord = mapper.readValue(resolvedBody, PIDRecord.class);
+        
+        assertEquals(createdRecord, resolvedRecord);
     }
 }
