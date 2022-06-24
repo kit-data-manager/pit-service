5b5c52d1e668559a2a18d064170501c8d2cc6eb8
Andreas Pfeil <andreas.pfeil@kit.edu>
2022-06-24 16:29:10 +0200

    Sonarlint

--- src/test/java/edu/kit/datamanager/pit/configuration/HandleProtocolSetupTest.java
+++ src/test/java/edu/kit/datamanager/pit/configuration/HandleProtocolSetupTest.java
@@ -6,12 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import edu.kit.datamanager.pit.SpringTestHelper;
 import edu.kit.datamanager.pit.configuration.ApplicationProperties.IdentifierSystemImpl;
@@ -19,7 +17,6 @@ import edu.kit.datamanager.pit.pidsystem.impl.HandleProtocolAdapter;
 import edu.kit.datamanager.pit.pidsystem.impl.HandleSystemRESTAdapter;
 import edu.kit.datamanager.pit.pidsystem.impl.InMemoryIdentifierSystem;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(
     properties = {
         //"pit.pidsystem.handle-protocol.credentials.user-handle = fakeUserHandle/forTesting",
