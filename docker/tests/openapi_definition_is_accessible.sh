0748177c674b2b814256c17a0ee304ad6ca8bc69
Andreas Pfeil <andreas.pfeil@kit.edu>
2022-07-22 12:53:48 +0200

    Add docker and container test setup.

---
+++ docker/tests/openapi_definition_is_accessible.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+response=$(curl --request GET \
+  --url http://localhost:8090/v3/api-docs \
+  --location \
+  --include)
+
+wasSuccessful=$(
+    echo $response \
+    | grep 200 \
+    | wc -l
+)
+
+if [ $wasSuccessful -eq 0 ]
+then
+    echo "FAILED: Expected HTTP 200 when accessing http://localhost:8090/v3/api-docs. Response:"
+    echo $response
+    exit 1  # failure
+else
+    echo "SUCCESS: OpenAPI definition is accessible"
+    exit 0  # success
+fi
