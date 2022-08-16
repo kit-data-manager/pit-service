82978e138553e915e6178276164a0c8db9f9ce97
Andreas Pfeil <andreas.pfeil@kit.edu>
2022-08-16 18:08:57 +0200

    Documentation and formatting.

--- src/main/java/edu/kit/datamanager/pit/pidlog/KnownPidsDao.java
+++ src/main/java/edu/kit/datamanager/pit/pidlog/KnownPidsDao.java
@@ -9,16 +9,36 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+/**
+ * Object to access known PIDs from the database.
+ * 
+ * Method implementation documentation is skipped due to automated
+ * implementation via spring data, documented in
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+ * 
+ * as well as the general concept documented in
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.core-concepts
+ */
 public interface KnownPidsDao extends JpaRepository<KnownPID, String>, JpaSpecificationExecutor<KnownPID> {
     Optional<KnownPID> findByPid(String pid);
+
     Collection<KnownPID> findDistinctPidsByCreated(Instant created);
+
     Collection<KnownPID> findDistinctPidsByModified(Instant modified);
+
     Collection<KnownPID> findDistinctPidsByCreatedBetween(Instant from, Instant to);
+
     Collection<KnownPID> findDistinctPidsByModifiedBetween(Instant from, Instant to);
+
     Page<KnownPID> findDistinctPidsByCreated(Instant created, Pageable pageable);
+
     Page<KnownPID> findDistinctPidsByModified(Instant modified, Pageable pageable);
+
     Page<KnownPID> findDistinctPidsByCreatedBetween(Instant from, Instant to, Pageable pageable);
+
     Page<KnownPID> findDistinctPidsByModifiedBetween(Instant from, Instant to, Pageable pageable);
+
     long countDistinctPidsByCreatedBetween(Instant from, Instant to);
+
     long countDistinctPidsByModifiedBetween(Instant from, Instant to);
 }
