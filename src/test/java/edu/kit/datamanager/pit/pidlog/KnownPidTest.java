f24c050c911d1c7d7cbf250fa8e2cb5ac0aa1edc
Andreas Pfeil <andreas.pfeil@kit.edu>
2022-08-30 16:25:36 +0200

    Add simple tests for KnownPids to be persisted.

---
+++ src/test/java/edu/kit/datamanager/pit/pidlog/KnownPidTest.java
@@ -0,0 +1,52 @@
+package edu.kit.datamanager.pit.pidlog;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.jupiter.api.Test;
+
+public class KnownPidTest {
+
+    private static final Instant NOW = Instant.now();
+    private static final Instant LATER = NOW.plus(1, ChronoUnit.DAYS);
+
+    @Test
+    void testContruction() {
+        KnownPid p = new KnownPid();
+        String pid = "test";
+        p.setPid(pid);
+        p.setCreated(NOW);
+        p.setModified(LATER);
+        assertEquals(pid, p.getPid());
+        assertEquals(NOW, p.getCreated());
+        assertEquals(LATER, p.getModified());
+        System.out.println(p.toString());
+    }
+
+    @Test
+    void testTrivialEquivalence() {
+        KnownPid p = new KnownPid();
+        assertNotEquals(null, p);
+        KnownPid b = new KnownPid();
+        assertEquals(p, b);
+    }
+
+    @Test
+    void testEquivalence() {
+        String pid = "test";
+        KnownPid p = new KnownPid(pid, NOW, LATER);
+        KnownPid b = new KnownPid(pid, NOW, LATER);
+        assertEquals(p, b);
+
+        b.setModified(NOW);
+        assertNotEquals(p, b);
+
+        b.setCreated(LATER);
+        assertNotEquals(p, b);
+    }
+}
