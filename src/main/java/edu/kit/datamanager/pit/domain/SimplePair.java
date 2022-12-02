53f459ba6082380426b628005e6e8af2ac488470
Andreas Pfeil <andreas.pfeil@kit.edu>
2022-12-02 17:05:00 +0100

    Add simple JSON representation for PID records.

---
+++ src/main/java/edu/kit/datamanager/pit/domain/SimplePair.java
@@ -0,0 +1,20 @@
+package edu.kit.datamanager.pit.domain;
+
+public class SimplePair {
+    private String key;
+
+    private String value;
+
+    SimplePair(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/edu/kit/datamanager/pit/domain/SimplePidRecord.java
@@ -0,0 +1,53 @@
+package edu.kit.datamanager.pit.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SimplePidRecord {
+
+    @JsonIgnore
+    public static final String CONTENT_TYPE = "application/vnd.datamanager.pid.simple";
+
+    private String pid;
+
+    @JsonProperty("record")
+    private List<SimplePair> pairs;
+
+    /**
+     * Converts a given PIDRecord representation into a SimplePidRecord
+     * representation.
+     * 
+     * @param rec
+     */
+    public SimplePidRecord(PIDRecord rec) {
+        this.pid = rec.getPid();
+        this.pairs = new ArrayList<>();
+        for (Entry<String, List<PIDRecordEntry>> entry : rec.getEntries().entrySet()) {
+            String key = entry.getKey();
+            for (PIDRecordEntry value : entry.getValue()) {
+                SimplePair p = new SimplePair(key, value.getValue());
+                this.pairs.add(p);
+            }
+        }
+    }
+
+    public List<SimplePair> getPairs() {
+        return pairs;
+    }
+
+    public void setPairs(List<SimplePair> pairs) {
+        this.pairs = pairs;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+}
