48a1b7ad66b96eb00b3a28c215768f5f6075da5b
Andreas Pfeil <andreas.pfeil@kit.edu>
2023-04-06 12:59:53 +0200

    Add comments to handle protocol credential values.

--- src/main/java/edu/kit/datamanager/pit/configuration/HandleCredentials.java
+++ src/main/java/edu/kit/datamanager/pit/configuration/HandleCredentials.java
@@ -1,6 +1,5 @@
 package edu.kit.datamanager.pit.configuration;
 
-
 import java.nio.file.Path;
 
 import javax.validation.constraints.Min;
@@ -11,25 +10,44 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
-
 @Validated
 @Configuration
 public class HandleCredentials {
+    /**
+     * The handle PID of the user / account to create PIDs.
+     */
     @NotBlank
     private String userHandle;
 
+    /**
+     * The prefix for the PIDs to be created.
+     */
     @NotBlank
     private String handleIdentifierPrefix;
 
+    /**
+     * The index of the user handle record, in which the public key is stored.
+     */
     @Min(1)
     private int privateKeyIndex = 300;
-    
+
+    /**
+     * The path on disk where the file containing the private key of the user is
+     * being stored. The file content may be encryptet or not.
+     */
     @NotNull
     private Path privateKeyPath;
-    
+
     // We get the passphrase out of an environment variable
     // instead of a path to a file containing the passphrase in a properties file.
     // Optional, as key might be unencrypted.
+    /**
+     * The passphrase for the key file stored at privateKeyPath.
+     * 
+     * We get the passphrase out of an environment variable instead of a path to a
+     * file containing the passphrase in a properties file. Optional, as key might
+     * be unencrypted.
+     */
     @Value("#{environment.handleProtocolPrivateKeyPassphrase}")
     private String privateKeyPassphrase;
 
