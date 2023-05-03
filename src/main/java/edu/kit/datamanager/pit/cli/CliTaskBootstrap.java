2b8f649a004d9edfb96504f51936e95203f9c914
Andreas Pfeil <andreas.pfeil@kit.edu>
2023-05-03 12:29:23 +0200

    feat(bootstrap): fill elasticsearch index

--- src/main/java/edu/kit/datamanager/pit/cli/CliTaskBootstrap.java
+++ src/main/java/edu/kit/datamanager/pit/cli/CliTaskBootstrap.java
@@ -2,17 +2,23 @@ package edu.kit.datamanager.pit.cli;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import edu.kit.datamanager.entities.messaging.PidRecordMessage;
 import edu.kit.datamanager.pit.common.InvalidConfigException;
 import edu.kit.datamanager.pit.configuration.ApplicationProperties;
+import edu.kit.datamanager.pit.domain.PIDRecord;
+import edu.kit.datamanager.pit.elasticsearch.PidRecordElasticRepository;
+import edu.kit.datamanager.pit.elasticsearch.PidRecordElasticWrapper;
 import edu.kit.datamanager.pit.pidlog.KnownPid;
 import edu.kit.datamanager.pit.pidlog.KnownPidsDao;
+import edu.kit.datamanager.pit.pitservice.ITypingService;
 import edu.kit.datamanager.service.IMessagingService;
 import edu.kit.datamanager.util.AuthenticationHelper;
 import edu.kit.datamanager.util.ControllerUtils;
@@ -21,16 +27,28 @@ public class CliTaskBootstrap implements ICliTask {
 
     private static final Logger LOG = LoggerFactory.getLogger(CliTaskBootstrap.class);
 
-    Stream<String> pids;
-    ApplicationProperties appProps;
-    KnownPidsDao knownPids;
-    IMessagingService messagingService;
+    protected Stream<String> pids;
+    protected ApplicationProperties appProps;
+    protected KnownPidsDao knownPids;
+    protected IMessagingService messagingService;
+    protected Optional<PidRecordElasticRepository> elastic;
+    protected ITypingService typingService;
 
     public CliTaskBootstrap(ConfigurableApplicationContext context, Stream<String> pids) {
         this.pids = pids;
         this.appProps = context.getBean(ApplicationProperties.class);
         this.knownPids = context.getBean(KnownPidsDao.class);
         this.messagingService = context.getBean(IMessagingService.class);
+        this.elastic = this.getBeanOptional(context, PidRecordElasticRepository.class);
+        this.typingService = context.getBean(ITypingService.class);
+    }
+
+    private <T> Optional<T> getBeanOptional(ConfigurableApplicationContext context, Class<T> clazz) {
+        try {
+            return Optional.of(context.getBean(clazz));
+        } catch (NoSuchBeanDefinitionException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
@@ -54,6 +72,17 @@ public class CliTaskBootstrap implements ICliTask {
                 );
                 LOG.info("Send PID {} to message broker.", known.getPid());
                 messagingService.send(message);
+                // store in Elasticsearch
+                elastic.ifPresent(elastic -> {
+                    try {
+                        PIDRecord rec = typingService.queryAllProperties(known.getPid());
+                        LOG.info("Store PID {} in Elasticsearch.", known.getPid());
+                        PidRecordElasticWrapper wrapper = new PidRecordElasticWrapper(rec, typingService.getOperations());
+                        elastic.save(wrapper);
+                    } catch (IOException e) {
+                        LOG.error("Failed to query PID {}.", known.getPid(), e);
+                    }
+                });
             });
         knownPids.flush();
         return false;
