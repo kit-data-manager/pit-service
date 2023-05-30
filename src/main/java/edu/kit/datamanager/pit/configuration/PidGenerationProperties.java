eec712fd9aa5285aa2c284a442c8828b658e242d
Andreas Pfeil <andreas.pfeil@kit.edu>
2023-05-30 18:03:04 +0200

    feat: add hex-chunks PID generator

--- src/main/java/edu/kit/datamanager/pit/configuration/PidGenerationProperties.java
+++ src/main/java/edu/kit/datamanager/pit/configuration/PidGenerationProperties.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 import edu.kit.datamanager.pit.pidgeneration.PidSuffixGenerator;
+import edu.kit.datamanager.pit.pidgeneration.generators.HexChunksGenerator;
 import edu.kit.datamanager.pit.pidgeneration.generators.PidSuffixGenLowerCase;
 import edu.kit.datamanager.pit.pidgeneration.generators.PidSuffixGenPrefixed;
 import edu.kit.datamanager.pit.pidgeneration.generators.PidSuffixGenUpperCase;
@@ -21,7 +22,8 @@ import edu.kit.datamanager.pit.pidgeneration.generators.PidSuffixGenUuid4;
 public class PidGenerationProperties {
 
     enum Mode {
-        UUID4
+        UUID4,
+        HEX_CHUNKS
     }
 
     enum Case {
@@ -33,6 +35,14 @@ public class PidGenerationProperties {
 
     @NotNull
     private Case casing = Case.LOWER;
+
+    /**
+     * If a CHUNK-based generation method is being used, this determines the amounts
+     * of chunks being used. Otherwise, this setting is being ignored.
+     * Chunk length depends on the generator. See their documentation for more
+     * information.
+     */
+    private int numChunks = 4;
 
     @NotNull
     private Optional<String> brandingPrefix = Optional.empty();
@@ -47,17 +57,16 @@ public class PidGenerationProperties {
     @Bean
     public PidSuffixGenerator pidGenerator() {
         PidSuffixGenerator generator = new PidSuffixGenUuid4();
-        // Add mode modes like this:
-        //if (this.mode == Mode.UUID4) {
-        //    generator = new PidSuffixGenUuid4();
-        //} else if (...) { ... }
-        
+        if (this.mode == Mode.HEX_CHUNKS) {
+            generator = new HexChunksGenerator(this.numChunks);
+        } // new generators have to be set up here
+
         if (casing == Case.LOWER) {
             generator = new PidSuffixGenLowerCase(generator);
         } else if (casing == Case.UPPER) {
             generator = new PidSuffixGenUpperCase(generator);
         }
-        
+
         // we assume the branding should not be affected
         // by the lower/upper case generators.
         if (brandingPrefix.isPresent()) {
@@ -86,9 +95,13 @@ public class PidGenerationProperties {
     public void setMode(Mode mode) {
         this.mode = mode;
     }
-    
+
     public void setCasing(Case lowerCase) {
         this.casing = lowerCase;
+    }
+
+    public void setNumChunks(int numChunks) {
+        this.numChunks = numChunks;
     }
 
 }
