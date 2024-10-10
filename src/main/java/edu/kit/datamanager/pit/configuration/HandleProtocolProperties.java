b2bc4f054d269b0b080c2fb876adbb914674cd27
Andreas Pfeil <andreas.pfeil@kit.edu>
2024-10-10 19:25:43 +0200

    feat: pit.pidsystem.handle-protocol.handleRedirectAttributes
    This allows to set a list (order matters) of attribute keys which shall be used as a source for the handle specific URL value. The URL value is a Handle value which defines where a user will be redirected when resolving the PID in the browser.

--- src/main/java/edu/kit/datamanager/pit/configuration/HandleProtocolProperties.java
+++ src/main/java/edu/kit/datamanager/pit/configuration/HandleProtocolProperties.java
@@ -1,7 +1,12 @@
 package edu.kit.datamanager.pit.configuration;
 
+import edu.kit.datamanager.pit.recordDecoration.CopyAttribute;
+import edu.kit.datamanager.pit.recordDecoration.RecordModifier;
+import jakarta.validation.constraints.NotNull;
+import net.handle.hdllib.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
@@ -10,6 +15,13 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static net.handle.hdllib.Common.STD_TYPE_URL;
 
 @Component
 @ConfigurationProperties("pit.pidsystem.handle-protocol")
@@ -23,10 +35,6 @@ public class HandleProtocolProperties {
 
     private static final Logger LOG = LoggerFactory.getLogger(HandleProtocolProperties.class);
 
-    public HandleProtocolProperties() {
-        LOG.info("Parse HANDLE_PROTOCOL configuration details.");
-    }
-
     // Without given credentials, PIDs can only be resolved.
     // Someone decided not to support Optional in the case of nested properties.
     // See: https://github.com/spring-projects/spring-boot/issues/15999
@@ -34,6 +42,22 @@ public class HandleProtocolProperties {
     @NestedConfigurationProperty
     @Nullable
     private HandleCredentials credentials;
+
+    @Value("#{${pit.pidsystem.handle-protocol.handleRedirectAttributes}}")
+    @NotNull
+    protected List<String> handleRedirectAttributes = List.of();
+
+    public HandleProtocolProperties() {
+        LOG.info("Parse HANDLE_PROTOCOL configuration details.");
+    }
+
+    public Collection<RecordModifier> getConfiguredModifiers() {
+        Set<RecordModifier> result = new HashSet<RecordModifier>();
+        if (!this.handleRedirectAttributes.isEmpty()) {
+            result.add(new CopyAttribute(this.handleRedirectAttributes, Util.decodeString(STD_TYPE_URL)));
+        }
+        return result;
+    }
 
     public HandleCredentials getCredentials() {
         return credentials;
