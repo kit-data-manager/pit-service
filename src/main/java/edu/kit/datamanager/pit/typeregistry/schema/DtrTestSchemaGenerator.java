7fdf5547c89796679b7748258f7f10bcd134c63d
Andreas Pfeil <andreas.pfeil@kit.edu>
2025-01-13 17:30:40 +0100

    refactor: use modern http client in dtr test schema generator

--- src/main/java/edu/kit/datamanager/pit/typeregistry/schema/DtrTestSchemaGenerator.java
+++ src/main/java/edu/kit/datamanager/pit/typeregistry/schema/DtrTestSchemaGenerator.java
@@ -5,26 +5,25 @@ import edu.kit.datamanager.pit.common.InvalidConfigException;
 import edu.kit.datamanager.pit.common.TypeNotFoundException;
 import edu.kit.datamanager.pit.configuration.ApplicationProperties;
 import jakarta.validation.constraints.NotNull;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 
 public class DtrTestSchemaGenerator implements SchemaGenerator {
 
     protected static final String ORIGIN = "dtr-test";
     protected final URI baseUrl;
-    protected final HttpClient http;
+    protected final RestClient http;
 
     public DtrTestSchemaGenerator(@NotNull ApplicationProperties props) {
         try {
@@ -32,56 +31,49 @@ public class DtrTestSchemaGenerator implements SchemaGenerator {
         } catch (URISyntaxException e) {
             throw new InvalidConfigException("BaseUrl not configured properly.");
         }
-        this.http = HttpClientBuilder.create()
-                .setRedirectStrategy(new LaxRedirectStrategy())
+        HttpClient httpClient = java.net.http.HttpClient.newBuilder()
+                .followRedirects(java.net.http.HttpClient.Redirect.NORMAL)
+                .build();
+        this.http = RestClient.builder()
+                .baseUrl(this.baseUrl.toString())
+                .requestFactory(new JdkClientHttpRequestFactory(httpClient))
                 .build();
     }
 
     @Override
     public SchemaInfo generateSchema(@NotNull String maybeTypePid) {
-        HttpGet request = new HttpGet(baseUrl.resolve("/" + maybeTypePid));
-        try {
-            return http.execute(
-                    request,
-                    response -> {
-                        int statusCode = response.getStatusLine().getStatusCode();
-                        if (statusCode / 100 == 2) {
-                            Schema schema = null;
-                            try (InputStream inputStream = response.getEntity().getContent()) {
-                                JSONObject jsonBody = new JSONObject(new JSONTokener(inputStream));
-                                JSONObject rawSchema = new JSONObject(new JSONTokener(jsonBody.getString("validationSchema")));
-                                schema = SchemaLoader.load(rawSchema);
-                            } catch (JSONException e) {
-                                return new SchemaInfo(
-                                        ORIGIN,
-                                        schema,
-                                        new ExternalServiceException(baseUrl.toString(), "No valid schema found resolving PID " + maybeTypePid)
-                                );
-                            }
-                            return new SchemaInfo(ORIGIN, schema, null);
-                        } else if (statusCode == 404) {
+        return this.http.get().uri(uriBuilder -> uriBuilder.pathSegment(maybeTypePid).build())
+                .exchange((request, response) -> {
+                    HttpStatusCode status = response.getStatusCode();
+                    if (status.is2xxSuccessful()) {
+                        Schema schema = null;
+                        try (InputStream inputStream = response.getBody()) {
+                            JSONObject jsonBody = new JSONObject(new JSONTokener(inputStream));
+                            JSONObject rawSchema = new JSONObject(new JSONTokener(jsonBody.getString("validationSchema")));
+                            schema = SchemaLoader.load(rawSchema);
+                        } catch (JSONException e) {
                             return new SchemaInfo(
                                     ORIGIN,
-                                    null,
-                                    new TypeNotFoundException(maybeTypePid)
-                            );
-                        } else {
-                            return new SchemaInfo(
-                                    ORIGIN,
-                                    null,
-                                    new ExternalServiceException(
-                                            this.baseUrl.toString(),
-                                            "Error generating schema: %s - %s".formatted(statusCode, response.getStatusLine().toString()))
+                                    schema,
+                                    new ExternalServiceException(baseUrl.toString(), "No valid schema found resolving PID " + maybeTypePid)
                             );
                         }
+                        return new SchemaInfo(ORIGIN, schema, null);
+                    } else if (status.value() == 404) {
+                        return new SchemaInfo(
+                                ORIGIN,
+                                null,
+                                new TypeNotFoundException(maybeTypePid)
+                        );
+                    } else {
+                        return new SchemaInfo(
+                                ORIGIN,
+                                null,
+                                new ExternalServiceException(
+                                        this.baseUrl.toString(),
+                                        "Error generating schema: %s - %s".formatted(status.value(), status.toString()))
+                        );
                     }
-            );
-        } catch (IOException e) {
-            return new SchemaInfo(
-                    ORIGIN,
-                    null,
-                    new ExternalServiceException(baseUrl.toString(), "Error communicating with service.", e)
-            );
-        }
+                });
     }
 }
