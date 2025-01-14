b12a4ddd798cb6093b35830682cbfd7671856ce6
Andreas Pfeil <andreas.pfeil@kit.edu>
2025-01-14 19:22:29 +0100

    fix(test): fix invalid values before validation of real record

--- src/test/java/edu/kit/datamanager/pit/web/RestWithHandleProtocolTest.java
+++ src/test/java/edu/kit/datamanager/pit/web/RestWithHandleProtocolTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -89,20 +90,26 @@ class RestWithHandleProtocolTest {
     }
 
     @Test
-    void testUpdateWithPidGiven() throws Exception {
-        // FIXME this test is currently not working as there is for some attributes no schema available in dtr-test,
-        //  and the type-api does currently not work.
-        String etag = this.mockMvc.perform(
-            get("/api/v1/pit/pid/21.11152/474a4b1c-de93-4d4a-b33d-1d32d63baf4b?validation=false")
+    void testDryrunUpdateWithPidGiven() throws Exception {
+        String url = "/api/v1/pit/pid/21.11152/474a4b1c-de93-4d4a-b33d-1d32d63baf4b";
+        MockHttpServletResponse response = this.mockMvc.perform(
+            get(url).param("validation", "false")
         )
             .andDo(MockMvcResultHandlers.print())
             .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
             .andReturn()
-                .getResponse()
-                .getHeader("ETag");
+            .getResponse();
+        String etag = response.getHeader("ETag");
+        PIDRecord record = mapper.readValue(response.getContentAsString(), PIDRecord.class);
+        // fix record, it is actually invalid...
+        record.removeAllValuesOf("URL");
+        String licenseUrl = "21.T11148/2f314c8fe5fb6a0063a8";
+        record.removeAllValuesOf(licenseUrl);
+        record.addEntry(licenseUrl, "{ \"licenseURL\": \"https://cdla.dev/permissive-2-0/\" }");
         this.mockMvc.perform(
-            put("/api/v1/pit/pid/21.11152/474a4b1c-de93-4d4a-b33d-1d32d63baf4b?dryrun=true")
-                .content("{ \"pid\": \"21.11152/474a4b1c-de93-4d4a-b33d-1d32d63baf4b\", \"entries\": { \"21.T11148/076759916209e5d62bd5\": [ { \"key\": \"21.T11148/076759916209e5d62bd5\", \"name\": \"kernelInformationProfile\", \"value\": \"21.T11148/b9b76f887845e32d29f7\" } ], \"21.T11148/397d831aa3a9d18eb52c\": [ { \"key\": \"21.T11148/397d831aa3a9d18eb52c\", \"name\": \"dateModified\", \"value\": \"2024-10-14T07:16:46+00:00\" } ], \"21.T11148/82e2503c49209e987740\": [ { \"key\": \"21.T11148/82e2503c49209e987740\", \"name\": \"checksum\", \"value\": \"{ \\\"sha256sum\\\": \\\"a92ad3bd2b0856b70d3f98cb2fa21964ea7f91218c46e327b65a0937c50a885c\\\" }\" } ], \"21.T11148/aafd5fb4c7222e2d950a\": [ { \"key\": \"21.T11148/aafd5fb4c7222e2d950a\", \"name\": \"dateCreated\", \"value\": \"2024-10-14T07:16:46+00:00\" } ], \"21.T11148/b8457812905b83046284\": [ { \"key\": \"21.T11148/b8457812905b83046284\", \"name\": \"digitalObjectLocation\", \"value\": \"https://paint-database.org/WRI1030197/WRI1030197-catalog-stac.json\" } ], \"21.T11148/1a73af9e7ae00182733b\": [ { \"key\": \"21.T11148/1a73af9e7ae00182733b\", \"name\": \"contact\", \"value\": \"https://orcid.org/0009-0007-0235-4995\" }, { \"key\": \"21.T11148/1a73af9e7ae00182733b\", \"name\": \"contact\", \"value\": \"https://orcid.org/0000-0002-2233-1041\" }, { \"key\": \"21.T11148/1a73af9e7ae00182733b\", \"name\": \"contact\", \"value\": \"https://orcid.org/0000-0001-9648-4385\" }, { \"key\": \"21.T11148/1a73af9e7ae00182733b\", \"name\": \"contact\", \"value\": \"https://orcid.org/0000-0002-9197-1739\" }, { \"key\": \"21.T11148/1a73af9e7ae00182733b\", \"name\": \"contact\", \"value\": \"https://orcid.org/0000-0002-4705-6285\" } ], \"21.T11148/2f314c8fe5fb6a0063a8\": [ { \"key\": \"21.T11148/2f314c8fe5fb6a0063a8\", \"name\": \"licenseURL\", \"value\": \"https://cdla.dev/permissive-2-0/\" } ], \"21.T11148/1c699a5d1b4ad3ba4956\": [ { \"key\": \"21.T11148/1c699a5d1b4ad3ba4956\", \"name\": \"digitalResourceType\", \"value\": \"application/json\" } ] }}")
+            put(url)
+                .param("dryrun", "true")
+                .content(mapper.writeValueAsString(record))
                 .contentType(ContentType.APPLICATION_JSON.getMimeType())
                 .header("If-Match", etag)
         )
