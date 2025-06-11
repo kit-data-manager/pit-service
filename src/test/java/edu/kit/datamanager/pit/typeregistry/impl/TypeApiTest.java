16423b235e8ff1e927c964ba00c5fbc3d3d60ed9
Andreas Pfeil <andreas.pfeil@kit.edu>
2025-06-11 16:21:25 +0200

    refactor: remove obsolete schema query test in TypeApiTest
    The type-api was fixed and in addition, the dtr-test instance has an ill-formed schema which is not accepted by our new validation library. Therefore, this test does not make sense anymore as anything else is covered by the other tests.

--- src/test/java/edu/kit/datamanager/pit/typeregistry/impl/TypeApiTest.java
+++ src/test/java/edu/kit/datamanager/pit/typeregistry/impl/TypeApiTest.java
@@ -2,15 +2,12 @@
 
 import edu.kit.datamanager.pit.configuration.ApplicationProperties;
 import edu.kit.datamanager.pit.typeregistry.AttributeInfo;
-import edu.kit.datamanager.pit.typeregistry.schema.SchemaInfo;
 import edu.kit.datamanager.pit.typeregistry.schema.SchemaSetGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Optional;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -49,23 +46,5 @@ class TypeApiTest {
         assertFalse(info.jsonSchema().isEmpty());
         assertTrue(info.name().contains("checksum"));
         assertEquals("PID-InfoType", info.typeName());
-    }
-
-    /* ================== TESTING INTERNALS ================== */
-
-    @Test
-    void querySchemaOfComplexType() {
-        // NOTE The new Type-API currently returns a malformed schema for the
-        //  checksum type in dtr-test. This test ensures that we at least get
-        //  the legacy schema in this case. If this test breaks, we either
-        //  have no schema, or the type-api is fixed.
-        Set<SchemaInfo> s = dtr.querySchemas(PID_COMPLEX_TYPE_CHECKSUM_DTRTEST);
-        assertEquals(2, s.size());
-        Optional<SchemaInfo> result = s.stream()
-                .filter(schemaInfo -> schemaInfo.schema() != null)
-                .filter(schemaInfo -> schemaInfo.error() == null)
-                .filter(schemaInfo -> schemaInfo.origin().contains("dtr-test"))
-                .findAny();
-        assertTrue(result.isPresent());
     }
 }
